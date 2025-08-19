cb2fa22da6e182b28a65aa5aec64c172b0734d27
Wasi Master <arianmollik323@gmail.com>
2025-08-20 05:51:02 +0600

    fix vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repository-name/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
